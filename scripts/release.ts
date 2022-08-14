7f727ff297ddd5f2e40b2184cf37b56b32eccf8f
mawencan <1819293933@qq.com>
2022-08-15 00:20:24 +0800

    chore: 添加打包和测试

---
+++ scripts/release.ts
@@ -0,0 +1,11 @@
+import { execSync } from 'child_process'
+import colors from 'colors'
+import { executeCommand } from '@abmao/pkgs'
+console.log(`${colors.cyan.bold('release: start')} 🏗`);
+(async function () {
+  execSync('npm run test', { stdio: 'inherit' })
+  execSync('npm run build', { stdio: 'inherit' })
+  await executeCommand('version')
+  await executeCommand('publish')
+})()
+console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
