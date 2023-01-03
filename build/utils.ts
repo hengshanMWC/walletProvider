fd638f01cc4c812e785b59724614577b99ec8307
mawencan <1044926436@qq.com>
2023-01-03 14:57:09 +0800

    init

--- build/utils.ts
+++
@@ -1,5 +0,0 @@
-import path from 'path'
-import { remove } from 'fs-extra'
-export function removeStructure() {
-  return remove(path.resolve(__dirname, '../dist'))
-}
--- scripts/release.ts
+++ scripts/release.ts
@@ -1,11 +1,11 @@
 import { execSync } from 'child_process'
 import colors from 'colors'
-import { executeCommand } from '@abmao/pkgs'
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
-  execSync('npm run test', { stdio: 'inherit' })
+  // 为了防止每次子包没有更新到pnpm-lock，报frozen-lockfile
+  execSync('pnpm i', { stdio: 'inherit' })
   execSync('npm run build', { stdio: 'inherit' })
-  await executeCommand('version')
-  await executeCommand('publish')
+  execSync('npm run version', { stdio: 'inherit' })
+  execSync('npm run publish', { stdio: 'inherit' })
 })()
 console.log(`${colors.cyan.bold('release: success')} 🎉🎉🎉🎉🎊`)
