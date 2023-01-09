2025fa5f70a63699984e1b5556757766da04ac34
mawencan <1044926436@qq.com>
2023-01-09 17:50:58 +0800

    chore: release命令添加测试

--- scripts/release.ts
+++ scripts/release.ts
@@ -4,6 +4,7 @@ console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
   // 为了防止每次子包没有更新到pnpm-lock，报frozen-lockfile
   execSync('pnpm i', { stdio: 'inherit' })
+  execSync('npm run test', { stdio: 'inherit' })
   execSync('npm run build', { stdio: 'inherit' })
   execSync('npm run version', { stdio: 'inherit' })
   execSync('npm run publish', { stdio: 'inherit' })
