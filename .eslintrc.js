876e8da61f71fe074014a3157347d78b1c1457e3
mawencan <1819293933@qq.com>
2023-10-18 15:35:11 +0800

    chore(eslint): 格式化规则调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,38 @@
 module.exports = {
-  extends: [
-    '@antfu/eslint-config-ts',
-  ],
+  root: true,
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  extends: ['@antfu/eslint-config-ts'],
   rules: {
-    '@typescript-eslint/no-var-requires': 'off',
+    'no-console': 'off',
+    'curly': 'off',
+    'no-new-func': 0,
+    'no-fallthrough': 'off',
+    'n/prefer-global/process': 'off',
+    'indent': 'off',
+    'brace-style': 'off',
+    'max-attributes-per-line': 'off',
+
+    'promise/param-names': 'off',
+
+    'jsdoc/check-alignment': 'off',
+
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-require-imports': 'off',
     '@typescript-eslint/ban-types': 'off',
-    'max-len': ['error', { code: 120 }],
-    'no-fallthrough': 'off',
-    'no-new-func': 0,
-    'no-console': 'off',
-    'n/prefer-global/process': 'off',
-    'jsdoc/check-alignment': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/brace-style': 'off',
+
+    'html/singleline-max-attributes': 'off',
+
+    '@stylistic/js/no-tabs': 'off',
+
+    // 希望打开的
+    'prefer-promise-reject-errors': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 }
--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,7 +1,10 @@
+import path from 'node:path'
 import { defineConfig } from 'vitepress'
+
 export default defineConfig({
   title: 'pkgs-template',
   description: 'pkgs模板',
+  outDir: path.join(__dirname, '../dist'),
   base: '/',
   themeConfig: {
     siteTitle: 'pkgs-template',
