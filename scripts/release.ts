81bdaea2eb544a25f4041b65b27807c7651d6915
hengshanMWC <1819293933@qq.com>
2023-12-25 17:45:58 +0800

    feat(init): vue

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,11 +1,8 @@
-import { execSync } from 'node:child_process'
 import colors from 'colors'
 import { commandPublish, commandVersion } from '@abmao/pkgs'
 
 console.log(`${colors.cyan.bold('release: start')} 🏗`);
 (async function () {
-  execSync('npm run test', { stdio: 'inherit' })
-  execSync('npm run build', { stdio: 'inherit' })
   await commandVersion()
   await commandPublish()
 })()
