ed43bdb83ba61cca6d0157c9b315daac544664f7
hengshanMWC <1819293933@qq.com>
2024-07-29 14:50:40 +0800

    chore(pkgs): 修改mode为diff

--- pkgs.config.ts
+++ pkgs.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from '@abmao/pkgs'
+import { Mode, defineConfig } from '@abmao/pkgs'
 
 // docs https://hengshanmwc.github.io/pkgs/docs/dist/config/
 export default defineConfig({
-  mode: 'diff',
+  mode: Mode.DIFF,
 })
